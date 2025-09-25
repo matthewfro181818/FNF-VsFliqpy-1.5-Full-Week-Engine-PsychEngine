00d5d68d4f8beff679d1ff61120d9e2ea33de5f2
matthewfro181818 <158477647+matthewfro181818@users.noreply.github.com>
2025-09-25 16:59:21 -0400

    Update LandLOLStage.hx

--- source/states/stages/LandLOLStage.hx
+++ source/states/stages/LandLOLStage.hx
@@ -3,30 +3,28 @@ package states.stages;
 import states.stages.objects.*;
 import substates.GameOverSubstate;
 import cutscenes.DialogueBox;
-
 import openfl.utils.Assets as OpenFlAssets;
 
 class LandLOLStage extends BaseStage {
 	// If you're moving your stage from PlayState to a stage file,
 	// you might have to rename some variables if they're missing, for example: camZooming -> game.camZooming
 	override function create() {
+		defaultCamZoom = 0.9;
+		curStage = 'land-lol';
 
-						defaultCamZoom = 0.9;
-						curStage = 'land-lol';
+		var bg:FlxSprite = new FlxSprite(-600, -200).loadGraphic(Paths.image('land-lol/stageback', 'week1'));
+		bg.antialiasing = true;
+		bg.scrollFactor.set(0.9, 0.9);
+		bg.active = false;
+		add(bg);
 
-						var bg:FlxSprite = new FlxSprite(-600, -200).loadGraphic(Paths.image('land-lol/stageback','week1'));
-						bg.antialiasing = true;
-						bg.scrollFactor.set(0.9, 0.9);
-						bg.active = false;
-						add(bg);
-	
-						var stageFront:FlxSprite = new FlxSprite(-650, 600).loadGraphic(Paths.image('land-lol/stagefront','week1'));
-						stageFront.setGraphicSize(Std.int(stageFront.width * 1.1));
-						stageFront.updateHitbox();
-						stageFront.antialiasing = true;
-						stageFront.scrollFactor.set(0.9, 0.9);
-						stageFront.active = false;
-						add(stageFront);
+		var stageFront:FlxSprite = new FlxSprite(-650, 600).loadGraphic(Paths.image('land-lol/stagefront', 'week1'));
+		stageFront.setGraphicSize(Std.int(stageFront.width * 1.1));
+		stageFront.updateHitbox();
+		stageFront.antialiasing = true;
+		stageFront.scrollFactor.set(0.9, 0.9);
+		stageFront.active = false;
+		add(stageFront);
 	}
 
 	var doof:DialogueBox = null;
