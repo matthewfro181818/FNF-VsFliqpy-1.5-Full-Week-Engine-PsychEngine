c413149a4682e4ef08e824fda0ca8acee7200690
matthewfro181818 <158477647+matthewfro181818@users.noreply.github.com>
2025-09-25 16:20:44 -0400

    Create LandDestroyedStage.hx

---
+++ source/states/stages/LandDestroyedStage.hx
@@ -0,0 +1,62 @@
+package states.stages;
+
+import states.stages.objects.*;
+import substates.GameOverSubstate;
+import cutscenes.DialogueBox;
+
+import openfl.utils.Assets as OpenFlAssets;
+
+class LandDestroyedStage extends BaseStage {
+	// If you're moving your stage from PlayState to a stage file,
+	// you might have to rename some variables if they're missing, for example: camZooming -> game.camZooming
+	override function create() {
+
+						defaultCamZoom = 0.9;
+						curStage = 'land-destroyed';
+
+						var bg:FlxSprite = new FlxSprite(-600, -200).loadGraphic(Paths.image('land-destroyed/stageback','week1'));
+						bg.antialiasing = true;
+						bg.scrollFactor.set(0.9, 0.9);
+						bg.active = false;
+						add(bg);
+	
+						var stageFront:FlxSprite = new FlxSprite(-650, 600).loadGraphic(Paths.image('land-destroyed/stagefront','week1'));
+						stageFront.setGraphicSize(Std.int(stageFront.width * 1.1));
+						stageFront.updateHitbox();
+						stageFront.antialiasing = true;
+						stageFront.scrollFactor.set(0.9, 0.9);
+						stageFront.active = false;
+						add(stageFront);
+	}
+
+	var doof:DialogueBox = null;
+
+	function initDoof() {
+		var file:String = Paths.txt('$songName/dialog'); // Checks for vanilla/Senpai dialogue
+		#if MODS_ALLOWED
+		if (!FileSystem.exists(file))
+		#else
+		if (!OpenFlAssets.exists(file))
+		#end
+		{
+			file = Paths.txt('$songName/dialog');
+		}
+
+		#if MODS_ALLOWED
+		if (!FileSystem.exists(file))
+		#else
+		if (!OpenFlAssets.exists(file))
+		#end
+		{
+			startCountdown();
+			return;
+		}
+
+		doof = new DialogueBox(false, CoolUtil.coolTextFile(file));
+		doof.cameras = [camHUD];
+		doof.scrollFactor.set();
+		doof.finishThing = startCountdown;
+		doof.nextDialogueThing = PlayState.instance.startNextDialogue;
+		doof.skipDialogueThing = PlayState.instance.skipDialogue;
+	}
+}
